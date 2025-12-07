48174eb26777840a61fe046569a33c7aa979e7f9
Edmund Huber <me@ehuber.info>
2025-12-07 14:45:19 -0800

    more squish

--- components/sdcard_interface/include/sdcard_interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef SDCARD_INTERFACE_H
-#define SDCARD_INTERFACE_H
-#include "esp_camera.h"
-
-void initialize_sdcard(void);
-void deinitialise_sdcard();
-void saveJpegToSdcard(camera_fb_t *);
-
-#endif //SDCARD_INTERFACE_H
